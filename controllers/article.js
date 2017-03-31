0bc78764348fa23f064b515f035c569841f13b83
stoyantodorovbg <stoyan_11@abv.bg>
2017-03-31 13:02:09 +0300

    initial commit

---
+++ controllers/article.js
@@ -0,0 +1,140 @@
+const Article = require('mongoose').model('Article');
+
+module.exports = {
+    createGet: (req, res) => {
+        res.render('article/create');
+    },
+
+    createPost: (req, res) => {
+        let articleParts = req.body;
+
+        let errorMsg = '';
+
+        if (!req.isAuthenticated()) {
+            errorMsg = 'You should be logged in to make articles!';
+        } else if (!articleParts.title) {
+            errorMsg = 'Invalid title!';
+        } else if (!articleParts.content) {
+            errorMsg = 'Invalid content!';
+        }
+        if (errorMsg) {
+            res.render('article/create', {error: errorMsg});
+            return;
+        }
+        let userId = req.user.id;
+        articleParts.author = userId;
+        Article.create(articleParts).then(article => {
+            req.user.articles.push(article.id);
+            req.user.save(err => {
+                if (err) {
+                    res.render('article/create', {error: err.message});
+                } else {
+                    res.redirect('/');
+                }
+            });
+        })
+    },
+    detailsGet: (req, res) => {
+        let id = req.params.id;
+
+        Article.findById(id).populate('author').then(article => {
+            if(!req.user){
+                res.render('article/details', {article: article, isUserAuthorized: false});
+                return;
+            }
+
+            req.user.isInRole('Admin').then(isAdmin => {
+                let isUserAuthorized = isAdmin || req.user.isAuthor(article);
+
+                res.render('article/details',{article: article, isUserAuthorized: isUserAuthorized});
+            });
+        });
+    },
+    editGet: (req, res) => {
+        let id = req.params.id;
+
+        if (!req.isAuthenticated()){
+            let returnUrl = `/article/edit/${id}`;
+            req.session.returnUrl = returnUrl;
+
+            res.redirect('/user/login');
+            return;
+        }
+
+        Article.findById(id).then(article => {
+            req.user.isInRole('Admin').then(isAdmin => {
+                if(!isAdmin && !req.user.isAuthor(article)){
+                    res.redirect('/');
+                    return;
+                }
+                res.render('article/edit', article)
+            });
+        });
+    },
+    editPost: (req, res) => {
+        let id = req.params.id;
+
+        let articleArgs = req.body;
+
+        let errorMsg = '';
+        if (!articleArgs.title){
+            errorMsg = 'Article title cannot be empty!';
+        } else if (!articleArgs.content){
+                errorMsg = 'Article content cannot be empty!';
+        }
+
+        if (errorMsg){
+            res.render('article/edit', {error: errorMsg})
+        } else {
+            Article.update({_id: id}, {$set: {title: articleArgs.title, content: articleArgs.content}})
+                .then(updateStatus => {
+                    res.redirect(`/article/details/${id}`);
+                })
+        }
+    },
+    deleteGet: (req, res) => {
+        let id = req.params.id;
+
+        if(!req.isAuthenticated()){
+            let returnUrl = `/article/delete/${id}`;
+            req.session.returUrl = returnUrl;
+
+            res.redirect('/user/login');
+            return;
+        }
+
+        Article.findById(id).then(article => {
+            req.user.isInRole('Admin').then(isAdmin => {
+                if (!isAdmin && !req.user.isAuthor(article)){
+                    res.redirect('/');
+                    return;
+                }
+                res.render('article/delete', article)
+            });
+        });
+    },
+    deletePost: (req, res) => {
+        let id = req.params.id;
+        Article.findOneAndRemove({_id: id}).populate('author').then(article => {
+            let author = article.author;
+
+            //Index of the article's ID in the author's articles.
+            let index = author.articles.indexOf(article.id);
+
+            if(index < 0){
+                let errorMsg = 'Article was not found for that author!';
+                res.render('article/delete', {error: errorMsg})
+            } else {
+                //Remove count element after givven index
+                let count = 1;
+                author.articles.splice(index, count)
+                author.save().then((user) => {
+                    res.redirect('/');
+                });
+
+            }
+
+        })
+    }
+
+};
---
+++ controllers/home.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Article = require('mongoose').model('Article');
+
+module.exports = {
+  index: (req, res) => {
+      Article.find({}).limit(6).populate('author').then(articles => {
+          res.render('home/index', {
+              articles: articles
+          });
+      });
+  }
+};
