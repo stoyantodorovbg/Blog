f6dfb53758834760819da459c78f03d7ed31b10c
Nikola Popow <nikola_ip@abv.bg>
2017-04-05 23:37:53 +0300

    nothing special

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const config = require('./config/config');
 const app = express();
 
-app.listen(3000);
+app.listen(3010);
 
 let env = 'development';
 require('./config/database')(config[env]);
