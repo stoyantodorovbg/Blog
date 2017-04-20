ddd82ec1b4925223fae377a3805a9705b15821f5
stoyantodorovbg <stoyan_11@abv.bg>
2017-04-20 19:09:07 +0300

    show the name of user in layOut

--- controllers/listing.js
+++ controllers/listing.js
@@ -4,16 +4,17 @@ const Listing = require('mongoose').model('Listing');
 module.exports = {
     listingsGet: (req, res) => {//
         Listing.find({}).populate('id').then(listings => {
-            res.render('ideas/ideas', {
+
+            res.render('listings/listings', {
                 listings: listings
             });
         });
     },
     createGet: (req, res) => {
-        res.render('about/ideasForm');
+        res.render('about/listingInput');
     },
     createPost: (req, res) => {
-        let listingParts = req.body
+        let listingParts = req.body;
         let errorMsg = '';
 
         if (!listingParts.content) {
@@ -28,7 +29,7 @@ module.exports = {
             listings.push(listing.id);
             listing.save(err => {
                 if (err) {
-                    res.render('about/ideasForm', {error: err.message});
+                    res.render('about/listingInput', {error: err.message});
                 } else {
                     res.render('about/sentListing');
                 }
