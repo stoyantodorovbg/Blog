b02b184f70864ffc252012e096ee0e2c875263fe
stoyantodorovbg <stoyan_11@abv.bg>
2017-04-21 18:06:41 +0300

    format date in News model

--- models/News.js
+++ models/News.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+let date = new Date();
+let dateString = date.toLocaleDateString()
 let newsSchema = mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     author: {type: String, required: true},
     source: {type: String, required: false},
-    date: {type: Date, default: new Date() },
+    date: {type: String, default: dateString},
     pathImage: [{type: String}],
     //id: { type: mongoose.Schema.Types.ObjectId}
 });
