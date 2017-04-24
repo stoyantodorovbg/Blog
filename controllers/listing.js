81cfff85dcccd911b30af418b72529fb71cb973e
stoyantodorovbg <stoyan_11@abv.bg>
2017-04-24 21:12:48 +0300

    add regex check for valid email and phone number at message and listing create function

--- controllers/listing.js
+++ controllers/listing.js
@@ -17,7 +17,17 @@ module.exports = {
         let listingParts = req.body;
         let errorMsg = '';
 
-        if (!listingParts.content) {
+        let phoneInput = listingParts.phone;
+        let rePhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+        let emailInput = listingParts.email;
+        let reEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])/;
+
+        if (emailInput.search(reEmail) == -1) {
+            errorMsg = emailInput + ' it is not valid email address!';
+        }else if (phoneInput.search(rePhone) == -1) {
+            errorMsg ='Please, insert valid phone number, using only digits!';
+        }else if (!listingParts.content) {
             errorMsg = 'Invalid content!';
         }else if (!listingParts.email) {
             errorMsg = 'Invalid email!';
