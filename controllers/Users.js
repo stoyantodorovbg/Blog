348237898f6abd5e7e91e649fea7bfa469ec831a
Nikola Popow <nikola_ip@abv.bg>
2017-04-26 16:47:12 +0300

    Users Page is functional and added videos to News

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
-const User = require('mongoose').model('User');
+const Users = require('mongoose').model('User');
 
 module.exports = {
-    usersGet: (req, res) => {//
-        User.find({}).populate('id').then(users => {
-
-            res.render('users/users', {
-                users: users
-            });
-        });
+    usersGet: (req, res) => {
+        res.render('Users/Users');
     },
 };
