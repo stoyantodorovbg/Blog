e9cb8ee84c9db2f0ed9330357e18d09590731d86
badjok3 <dani.popoff@abv.bg>
2017-04-26 17:06:14 +0300

    fixed bug where users button crashed the whole site. made profile edit work properly and display the information right after you've given it. made a few styling changes.

--- controllers/Users.js
+++ controllers/Users.js
@@ -2,12 +2,7 @@ const mongoose = require('mongoose');
 const User = require('mongoose').model('User');
 
 module.exports = {
-    usersGet: (req, res) => {//
-        User.find({}).populate('id').then(users => {
-
-            res.render('users/users', {
-                users: users
-            });
-        });
+    usersGet: (req, res) => {
+            res.render('Users/Users');
     },
 };
