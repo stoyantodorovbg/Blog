fedd38d96fe58e06c19ddc40c45f289fe4db558a
stoyantodorovbg <stoyan_11@abv.bg>
2017-04-27 19:57:33 +0300

    add users page

--- controllers/Users.js
+++ controllers/Users.js
@@ -3,6 +3,20 @@ const User = require('mongoose').model('User');
 
 module.exports = {
     usersGet: (req, res) => {
-            res.render('Users/Users');
+        User.find({}).then(users => {
+
+            res.render('Users/Users', {
+                users: users
+            });
+        });
+    },
+    detailsGet: (req, res) => {
+        let id = req.params.id;
+
+        User.findById(id).populate('profile').then(user => {
+
+            res.render('Users/details', {user: user});
+
+        });
     },
 };
